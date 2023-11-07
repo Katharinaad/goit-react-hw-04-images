af8a1fd8f7e58cfeb4a9afc853650eada878dcfb
Katharinaad <vermilion.adams@gmail.com>
2023-11-07 21:08:58 +0100

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,16 +2,19 @@ import { ImageGalleryItem } from './ImageGalleryItem';
 import css from './ImageGallery.module.css';
 
 export function ImageGallery({ pictures, onClick }) {
+  const showPics = Array.isArray(pictures) && pictures.length;
+
   return (
     <ul className={css.gallery}>
-      {pictures.map(picture => (
-        <ImageGalleryItem
-          key={picture.id}
-          pictureUrl={picture.webformatURL}
-          pictureLarge={picture.largeImageURL}
-          onClick={onClick}
-        />
-      ))}
+      {showPics > 0 &&
+        pictures.map((picture, index) => (
+          <ImageGalleryItem
+            key={index}
+            pictureUrl={picture.webformatURL}
+            pictureLarge={picture.largeImageURL}
+            onClick={onClick}
+          />
+        ))}
     </ul>
   );
 }
