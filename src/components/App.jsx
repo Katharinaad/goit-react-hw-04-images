ac965d1e282748cf3f309c09f0886b242cd179ca
Katharinaad <vermilion.adams@gmail.com>
2023-11-07 21:08:56 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import 'react-notifications/lib/notifications.css';
 import API from '../services/api';
 import { ImageGallery } from './ImageGallery/ImageGallery';
@@ -11,111 +11,207 @@ import {
   NotificationContainer,
 } from 'react-notifications';
 
-export class App extends Component {
-  state = {
-    searchedQuery: '',
-    page: 1,
-    pictures: [],
-    isLoading: false,
-    error: null,
-    isEnd: false,
-    isModalOpen: false,
-    largeImageURL: '',
+export function App() {
+  const [searchedQuery, setSearchedQuery] = useState('');
+  const [page, setPage] = useState('');
+  const [pictures, setPictures] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState(null);
+  const [isEnd, setIsEnd] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [largeImageURL, setLargeImageURL] = useState('');
+
+  //   async componentDidUpdate(_, prevState) {
+  //     const { page, searchedQuery } = this.state;
+  //     if (prevState.page !== page || prevState.searchedQuery !== searchedQuery) {
+  //       this.setState({ isLoading: true });
+
+  //       try {
+  //         const response = await API.fetchPics(searchedQuery, page);
+
+  //         this.setState(prevState => ({
+  //           pictures: [...prevState.pictures, ...response.hits],
+  //         }));
+
+  //         if (!response.totalHits) {
+  //           return NotificationManager.error('No images found');
+  //         }
+
+  //         const totalPages = Math.ceil(response.totalHits / 12);
+
+  //         if (page === totalPages) {
+  //           this.setState({ isEnd: true });
+  //           return NotificationManager.info('End of search');
+  //         }
+  //       } catch (error) {
+  //         return NotificationManager.error('Error fetching images');
+  //       } finally {
+  //         this.setState({ isLoading: false });
+  //       }
+  //     }
+  //   }
+
+  const handleSearchSubmit = searchedQuery => {
+    setSearchedQuery(searchedQuery);
+    setPictures([]);
+    setPage(1);
   };
 
-  async componentDidUpdate(_, prevState) {
-    const { page, searchedQuery } = this.state;
-    if (prevState.page !== page || prevState.searchedQuery !== searchedQuery) {
-      this.setState({ isLoading: true });
-
-      try {
-        const response = await API.fetchPics(searchedQuery, page);
-
-        this.setState(prevState => ({
-          pictures: [...prevState.pictures, ...response.hits],
-        }));
-
-        if (!response.totalHits) {
-          return NotificationManager.error('No images found');
-        }
-
-        const totalPages = Math.ceil(response.totalHits / 12);
-
-        if (page === totalPages) {
-          this.setState({ isEnd: true });
-          return NotificationManager.info('End of search');
-        }
-      } catch (error) {
-        return NotificationManager.error('Error fetching images');
-      } finally {
-        this.setState({ isLoading: false });
-      }
-    }
-  }
-
-  handleSearchSubmit = searchedQuery => {
-    this.setState({ searchedQuery, pictures: [], page: 1 });
+  const loadMore = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
-  loadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+  const onOpenModal = url => {
+    setIsModalOpen(true);
+    setLargeImageURL(url);
   };
 
-  onOpenModal = url => {
-    this.setState({
-      isModalOpen: true,
-      largeImageURL: url,
-    });
+  const onCloseModal = () => {
+    setIsModalOpen(false);
+    setLargeImageURL('');
   };
 
-  onCloseModal = () => {
-    this.setState({
-      isModalOpen: false,
-      largeImageURL: '',
-    });
-  };
+  // const showPics = Array.isArray(pictures) && pictures.length;
 
-  render() {
-    const showPics =
-      Array.isArray(this.state.pictures) && this.state.pictures.length;
-
-    return (
-      <div className="container">
-        <Searchbar onSubmit={this.handleSearchSubmit} />
-        {showPics > 0 && (
-          <ImageGallery
-            pictures={this.state.pictures}
-            onClick={this.onOpenModal}
+  return (
+    <div className="container">
+      <Searchbar onSubmit={handleSearchSubmit} />
+      <ImageGallery pictures={pictures} onClick={onOpenModal} />
+      {pictures > 0 && !isEnd && <Button onClick={loadMore} />}
+      {isLoading && (
+        <div
+          style={{
+            position: 'fixed',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+          }}
+        >
+          <RotatingLines
+            strokeColor="grey"
+            strokeWidth="5"
+            animationDuration="0.75"
+            width="45"
+            visible={true}
           />
-        )}
-        {showPics && !this.state.isEnd && <Button onClick={this.loadMore} />}
-        {this.state.isLoading && (
-          <div
-            style={{
-              position: 'fixed',
-              top: '50%',
-              left: '50%',
-              transform: 'translate(-50%, -50%)',
-            }}
-          >
-            <RotatingLines
-              strokeColor="grey"
-              strokeWidth="5"
-              animationDuration="0.75"
-              width="45"
-              visible={true}
-            />
-          </div>
-        )}
-        {this.state.isModalOpen && (
-          <Modal onCloseModal={this.onCloseModal}>
-            <img src={this.state.largeImageURL} alt="" />
-          </Modal>
-        )}
-        <NotificationContainer />
-      </div>
-    );
-  }
+        </div>
+      )}
+      {isModalOpen && (
+        <Modal onCloseModal={onCloseModal}>
+          <img src={largeImageURL} alt="" />
+        </Modal>
+      )}
+      <NotificationContainer />
+    </div>
+  );
 }
+
+// state = {
+//   searchedQuery: '',
+//   page: 1,
+//   pictures: [],
+//   isLoading: false,
+//   error: null,
+//   isEnd: false,
+//   isModalOpen: false,
+//   largeImageURL: '',
+// };
+
+//   async componentDidUpdate(_, prevState) {
+//     const { page, searchedQuery } = this.state;
+//     if (prevState.page !== page || prevState.searchedQuery !== searchedQuery) {
+//       this.setState({ isLoading: true });
+
+//       try {
+//         const response = await API.fetchPics(searchedQuery, page);
+
+//         this.setState(prevState => ({
+//           pictures: [...prevState.pictures, ...response.hits],
+//         }));
+
+//         if (!response.totalHits) {
+//           return NotificationManager.error('No images found');
+//         }
+
+//         const totalPages = Math.ceil(response.totalHits / 12);
+
+//         if (page === totalPages) {
+//           this.setState({ isEnd: true });
+//           return NotificationManager.info('End of search');
+//         }
+//       } catch (error) {
+//         return NotificationManager.error('Error fetching images');
+//       } finally {
+//         this.setState({ isLoading: false });
+//       }
+//     }
+//   }
+
+//   handleSearchSubmit = searchedQuery => {
+//     this.setState({ searchedQuery, pictures: [], page: 1 });
+//   };
+
+//   loadMore = () => {
+//     this.setState(prevState => ({
+//       page: prevState.page + 1,
+//     }));
+//   };
+
+//   onOpenModal = url => {
+//     this.setState({
+//       isModalOpen: true,
+//       largeImageURL: url,
+//     });
+//   };
+
+//   onCloseModal = () => {
+//     this.setState({
+//       isModalOpen: false,
+//       largeImageURL: '',
+//     });
+//   };
+
+//   render() {
+//     const showPics =
+//       Array.isArray(this.state.pictures) && this.state.pictures.length;
+
+//     return (
+//       <div className="container">
+//         <Searchbar onSubmit={this.handleSearchSubmit} />
+//         {showPics > 0 && (
+//           <ImageGallery
+//             pictures={this.state.pictures}
+//             onClick={this.onOpenModal}
+//           />
+//         )}
+//         {showPics > 0 && !this.state.isEnd && (
+//           <Button onClick={this.loadMore} />
+//         )}
+//         {this.state.isLoading && (
+//           <div
+//             style={{
+//               position: 'fixed',
+//               top: '50%',
+//               left: '50%',
+//               transform: 'translate(-50%, -50%)',
+//             }}
+//           >
+//             <RotatingLines
+//               strokeColor="grey"
+//               strokeWidth="5"
+//               animationDuration="0.75"
+//               width="45"
+//               visible={true}
+//             />
+//           </div>
+//         )}
+//         {this.state.isModalOpen && (
+//           <Modal onCloseModal={this.onCloseModal}>
+//             <img src={this.state.largeImageURL} alt="" />
+//           </Modal>
+//         )}
+//         <NotificationContainer />
+//       </div>
+//     );
+//   }
+// }
