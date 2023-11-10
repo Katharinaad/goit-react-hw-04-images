7132acc50e560337f4776b03376b2e0e17c7f390
Katharinaad <vermilion.adams@gmail.com>
2023-11-10 11:10:14 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,8 +16,6 @@ export function App() {
   const [pictures, setPictures] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isEnd, setIsEnd] = useState(false);
-  // const [isModalOpen, setIsModalOpen] = useState(false);
-  // const [largeImageURL, setLargeImageURL] = useState('');
 
   useEffect(() => {
     if (!searchedQuery) return;
@@ -59,16 +57,6 @@ export function App() {
     setPage(prevPage => prevPage + 1);
   };
 
-  // const onOpenModal = url => {
-  //   setIsModalOpen(true);
-  //   setLargeImageURL(url);
-  // };
-
-  // const onCloseModal = () => {
-  //   setIsModalOpen(false);
-  //   setLargeImageURL('');
-  // };
-
   return (
     <div className="container">
       <Searchbar onSubmit={handleSearchSubmit} />
@@ -96,113 +84,3 @@ export function App() {
     </div>
   );
 }
-
-// state = {
-//   searchedQuery: '',
-//   page: 1,
-//   pictures: [],
-//   isLoading: false,
-//   error: null,
-//   isEnd: false,
-//   isModalOpen: false,
-//   largeImageURL: '',
-// };
-
-//   async componentDidUpdate(_, prevState) {
-//     const { page, searchedQuery } = this.state;
-//     if (prevState.page !== page || prevState.searchedQuery !== searchedQuery) {
-//       this.setState({ isLoading: true });
-
-//       try {
-//         const response = await API.fetchPics(searchedQuery, page);
-
-//         this.setState(prevState => ({
-//           pictures: [...prevState.pictures, ...response.hits],
-//         }));
-
-//         if (!response.totalHits) {
-//           return NotificationManager.error('No images found');
-//         }
-
-//         const totalPages = Math.ceil(response.totalHits / 12);
-
-//         if (page === totalPages) {
-//           this.setState({ isEnd: true });
-//           return NotificationManager.info('End of search');
-//         }
-//       } catch (error) {
-//         return NotificationManager.error('Error fetching images');
-//       } finally {
-//         this.setState({ isLoading: false });
-//       }
-//     }
-//   }
-
-//   handleSearchSubmit = searchedQuery => {
-//     this.setState({ searchedQuery, pictures: [], page: 1 });
-//   };
-
-//   loadMore = () => {
-//     this.setState(prevState => ({
-//       page: prevState.page + 1,
-//     }));
-//   };
-
-//   onOpenModal = url => {
-//     this.setState({
-//       isModalOpen: true,
-//       largeImageURL: url,
-//     });
-//   };
-
-//   onCloseModal = () => {
-//     this.setState({
-//       isModalOpen: false,
-//       largeImageURL: '',
-//     });
-//   };
-
-//   render() {
-//     const showPics =
-//       Array.isArray(this.state.pictures) && this.state.pictures.length;
-
-//     return (
-//       <div className="container">
-//         <Searchbar onSubmit={this.handleSearchSubmit} />
-//         {showPics > 0 && (
-//           <ImageGallery
-//             pictures={this.state.pictures}
-//             onClick={this.onOpenModal}
-//           />
-//         )}
-//         {showPics > 0 && !this.state.isEnd && (
-//           <Button onClick={this.loadMore} />
-//         )}
-//         {this.state.isLoading && (
-//           <div
-//             style={{
-//               position: 'fixed',
-//               top: '50%',
-//               left: '50%',
-//               transform: 'translate(-50%, -50%)',
-//             }}
-//           >
-//             <RotatingLines
-//               strokeColor="grey"
-//               strokeWidth="5"
-//               animationDuration="0.75"
-//               width="45"
-//               visible={true}
-//             />
-//           </div>
-//         )}
-//         {this.state.isModalOpen && (
-//           <Modal onCloseModal={this.onCloseModal}>
-//             <img src={this.state.largeImageURL} alt="" />
-//           </Modal>
-//         )}
-//         <NotificationContainer />
-//       </div>
-//     );
-//   }
-// }
