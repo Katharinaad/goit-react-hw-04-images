286535c78c18ad22d008fd59c46e01bb30d5aae7
Katharinaad <vermilion.adams@gmail.com>
2023-11-10 11:07:49 +0100

    Update ImageGalleryItem.jsx

--- src/components/ImageGallery/ImageGalleryItem.jsx
+++ src/components/ImageGallery/ImageGalleryItem.jsx
@@ -1,14 +1,23 @@
+import Modal from 'components/Modal/Modal';
 import css from './ImageGalleryItem.module.css';
+import { useState } from 'react';
 
-export function ImageGalleryItem({ pictureUrl, pictureLarge, onClick }) {
+export function ImageGalleryItem({ pictureUrl, pictureLarge }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <li className={css.galleryList}>
       <img
         className={css.galleryPic}
         src={pictureUrl}
         alt=""
-        onClick={() => onClick(pictureLarge)}
+        onClick={() => setIsModalOpen(true)}
       />
+      {isModalOpen && (
+        <Modal onCloseModal={() => setIsModalOpen(false)}>
+          <img src={pictureLarge} alt="" />
+        </Modal>
+      )}
     </li>
   );
 }
